6d8519cd84b712dfa253cc14865a64a807b37b24
李争献 <lizx3@ucweb.com>
2015-11-12 15:21:51 +0800

    主页除了喜欢喝不喜欢以外都有相应

--- app/src/main/java/lee/scut/edu/mycook/activity/RecommendActivity.java
+++ app/src/main/java/lee/scut/edu/mycook/activity/RecommendActivity.java
@@ -1,7 +1,8 @@
 package lee.scut.edu.mycook.activity;
 
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 import lee.scut.edu.mycook.R;
+import lee.scut.edu.mycook.entity.FoodDetails.Food;
 import lee.scut.edu.mycook.entity.FoodLists.FoodListItem;
 import lee.scut.edu.mycook.entity.FoodLists.FoodRecommend;
 
@@ -27,8 +29,10 @@ import lee.scut.edu.mycook.entity.FoodLists.FoodRecommend;
  * Created by jsonlee on 11/12/15.
  */
 public class RecommendActivity extends BaseActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
-    Button btnYule,btnPersonal,btnShop;
-    ListView lvRecommend,lvILike;
+    Button btnCaipu, btnYule, btnPersonal, btnShop;
+    Button btnLike, btnDislike, btnLiked;
+    ListView lvRecommend, lvILike;
+    ImageView ivBigRecmd;
     private List<Map<String, Object>> data;
 
     @Override
@@ -36,24 +40,34 @@ public class RecommendActivity extends BaseActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
 
-        btnYule = (Button)findViewById(R.id.btnYuLe);
-        btnPersonal = (Button)findViewById(R.id.btnPersonal);
-        btnShop = (Button)findViewById(R.id.btnShop);
-        lvRecommend = (ListView)findViewById(R.id.listRecommend);
-        lvILike = (ListView)findViewById(R.id.list_i_like);
+        btnCaipu = (Button) findViewById(R.id.btnCaipu);
+        btnYule = (Button) findViewById(R.id.btnYuLe);
+        btnPersonal = (Button) findViewById(R.id.btnPersonal);
+        btnShop = (Button) findViewById(R.id.btnShop);
+        btnLike = (Button) findViewById(R.id.btnLike);
+        btnLiked = (Button) findViewById(R.id.btnLiked);
+        btnDislike = (Button) findViewById(R.id.btnDislike);
+        lvRecommend = (ListView) findViewById(R.id.listRecommend);
+        lvILike = (ListView) findViewById(R.id.list_i_like);
+        ivBigRecmd = (ImageView) findViewById(R.id.ivBigRecmd);
 
+        btnCaipu.setOnClickListener(this);
         btnYule.setOnClickListener(this);
         btnPersonal.setOnClickListener(this);
         btnShop.setOnClickListener(this);
+        btnLike.setOnClickListener(this);
+        btnDislike.setOnClickListener(this);
+        btnLiked.setOnClickListener(this);
+        ivBigRecmd.setOnClickListener(this);
         lvRecommend.setOnItemClickListener(this);
         lvILike.setOnItemClickListener(this);
 
         data = new ArrayList<Map<String, Object>>();
         for (FoodListItem food : offlineData.foodLists.allFoods) {
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("fileName", food.name);
-                map.put("id",food.id);
-                data.add(map);
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("fileName", food.name);
+            map.put("id", food.id);
+            data.add(map);
         }
         adapterILike = new SimpleAdapter(this, data,
                 R.layout.item_food_name_text,
@@ -61,20 +75,64 @@ public class RecommendActivity extends BaseActivity implements View.OnClickListe
         lvILike.setAdapter(adapterILike);
 
         lvRecommend.setAdapter(adapterRecommend);
+
+        lvRecommend.performItemClick(null, 0, lvRecommend.getItemIdAtPosition(0));
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.ivBigRecmd:
+                jumpToActivity(FoodDetailActivity.class);
+                break;
+            case R.id.btnCaipu:
+                jumpToActivity(FoodListActivity.class);
+                break;
+            case R.id.btnYuLe:
+                jumpToActivity(PlayerActivity.class);
+                break;
+            case R.id.btnShop:
+                Intent in = new Intent(Intent.ACTION_VIEW);
+                in.setData(Uri.parse("http://www.taobao.com"));
+                startActivity(in);
+                break;
+            case R.id.btnPersonal:
+                jumpToActivity(PersonalActivity.class);
+                break;
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        if (parent.getId() == lvILike.getId()) {
+            FoodDetailActivity.foodId = (int) data.get(position).get("id");
+            jumpToActivity(FoodDetailActivity.class);
+        } else if (parent.getId() == lvRecommend.getId()) {
+            FoodDetailActivity.foodId = (int) id;
+            Food food = offlineData.getFoodById((int) id);
+            ivBigRecmd.setImageBitmap(BitmapFactory.decodeFile(food.picUrl));
+            setLikedButtons(food.isFavorite);
+        }
     }
+
+    /**
+     * @param like true显示已喜欢按钮，false显示两个按钮选项
+     */
+    public void setLikedButtons(boolean like) {
+        if (like) {
+            btnLiked.setVisibility(View.VISIBLE);
+            btnLike.setVisibility(View.GONE);
+            btnDislike.setVisibility(View.GONE);
+        } else {
+            btnLiked.setVisibility(View.GONE);
+            btnLike.setVisibility(View.VISIBLE);
+            btnDislike.setVisibility(View.VISIBLE);
+        }
+    }
+
     private SimpleAdapter adapterILike;
 
-    private BaseAdapter adapterRecommend = new BaseAdapter(){
+    private BaseAdapter adapterRecommend = new BaseAdapter() {
 
         @Override
         public int getCount() {
@@ -94,24 +152,23 @@ public class RecommendActivity extends BaseActivity implements View.OnClickListe
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder;
-            if(convertView == null)
-            {
+            if (convertView == null) {
                 holder = new ViewHolder();
                 LayoutInflater inflater = getLayoutInflater();
-                convertView = inflater.inflate(R.layout.item_food_recommend,null);
+                convertView = inflater.inflate(R.layout.item_food_recommend, null);
                 holder.tvName = (TextView) convertView.findViewById(R.id.tvFoodName);
                 holder.ivRecmd = (ImageView) convertView.findViewById(R.id.ivFood);
                 convertView.setTag(holder);
-            }
-            else{
-                holder = (ViewHolder)convertView.getTag();
+            } else {
+                holder = (ViewHolder) convertView.getTag();
             }
             FoodRecommend foodRecommend = (FoodRecommend) getItem(position);
             holder.ivRecmd.setImageBitmap(BitmapFactory.decodeFile(foodRecommend.picUrl));
             holder.tvName.setText(foodRecommend.name);
             return convertView;
         }
-        class ViewHolder{
+
+        class ViewHolder {
             ImageView ivRecmd;
             TextView tvName;
         }
