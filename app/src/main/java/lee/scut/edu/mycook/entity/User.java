81cb06df1570009e17e12efe70fe7299f504ee36
李争献 <lizx3@ucweb.com>
2015-11-16 10:30:22 +0800

    字体变很大 

---
+++ app/src/main/java/lee/scut/edu/mycook/entity/User.java
@@ -0,0 +1,14 @@
+package lee.scut.edu.mycook.entity;
+
+import java.util.Date;
+
+/**
+ * Created by jsonlee on 11/12/15.
+ */
+public class User {
+    String username;
+    String passwdHash;
+    String gender;
+    String city;
+    Date birthday;
+}
